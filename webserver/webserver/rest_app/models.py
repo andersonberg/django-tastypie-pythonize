9a95cce35a36c24102b2221d3d77170696ca0003
Anderson Berg <andersonberg@gmail.com>
2014-10-03 20:45:49 -0300

    adicionando rest_app ao settings.py; criando models

--- webserver/webserver/rest_app/models.py
+++ webserver/webserver/rest_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from tastypie.utils.timezone import now
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User)
+    date_created = models.DateTimeField(default=now)
+    text = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+
+    def __unicode__(self):
+        return self.text
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.text)[:50]
+        return super(Post, self).save(*args, **kwargs)
